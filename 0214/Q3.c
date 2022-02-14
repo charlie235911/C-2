43de0d0f51cffb6b29bc3b115b912074df0f7920
charlie235911 <biau235911@gmail.com>
2022-02-15 00:31:14 +0800

    add 0214

--- 0214/Q3.c
+++ 0214/Q3.c
@@ -2,42 +2,38 @@
 
 int main(void)
 {
-    int m, a, n[100], p[100][100], length[100], ans;
+    int m = 0, n = 0, p[100] = {0}, length[10] = {0}, tempt = 1;
     scanf("%d", &m);
     for (int i = 0; i < m; i++)
     {
-        scanf("%d", &n[i]);
-        for (int j = 0; j < n[i]; j++)
+        scanf("%d", &n);
+        for (int j = 0; j < n; j++)
         {
-            scanf("%d", &p[i][j]);
+            scanf("%d", &p[j]);
+        }
+        for (int k = 1; k < n; k++)
+        {
+            if (p[k] != p[k - 1])
+            {
+                if (tempt > length[i])
+                {
+                    length[i] = tempt;
+                }
+                tempt = 0;
+                tempt++;
+            }
+            else if (p[k] == p[k - 1])
+            {
+                tempt++;
+                if (tempt == n)
+                {
+                    length[i] = tempt;
+                }
+            }
         }
     }
-    for (int i = 0; i < m; i++)
+    for (int x = 0; x < m; x++)
     {
-        for (int c = 0; c < 100; c++)
-        {
-            length[c] = 0;
-        }
-        for (int j = 0, a = 0; j < n[i]; j++)
-        {
-            if (p[i][j] == p[i][j - 1])
-            {
-                length[a]++;
-            }
-            else if (p[i][j] != p[i][j - 1])
-            {
-                a++;
-                length[a]++;
-            }
-        }
-        for (int k = 0; k < a; k++)
-        {
-            if (length[k] > length[0])
-            {
-                length[0] = length[k];
-            }
-        }
-        ans = length[0];
-        printf("%d\n", ans);
+        printf("%d\n", length[x]);
     }
 }
