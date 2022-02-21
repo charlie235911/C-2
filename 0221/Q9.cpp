15504ac61c168cc8175cde43d7d3336a95cab4fc
charlie235911 <biau235911@gmail.com>
2022-02-21 14:58:51 +0800

    add 0221

--- 0221/Q9.cpp
+++ 0221/Q9.cpp
@@ -1,22 +1,45 @@
 #include <iostream>
 using namespace std;
 
+int test(int length, int f)
+{
+    while (f != 1)
+    {
+        if (f % 2 == 0)
+        {
+            f = f / 2;
+            length++;
+        }
+        else
+        {
+            f = 3 * f + 1;
+            length++;
+        }
+    }
+    return length;
+}
+
 int main()
 {
-    int n, i[100000], j[100000];
+    int n, i[100], j[100], ans = 0, length;
     cin >> n;
     for (int a = 0; a < n; a++)
     {
-        cin >> i[a];
-        cin >> j[a];
+        cin >> i[a] >> j[a];
     }
-    for (int a = 0; a < n; a++)
+    for (int b = 0; b < n; b++)
     {
-        if (a[i])
+        int num1 = i[b], num2 = j[b];
+        for (int f = num1; f <= num2; f++)
         {
-            /* code */
+            length = 1;
+            test(length, f);
+            if (length > ans)
+            {
+                ans = length;
+            }
         }
-        
+        cout << i[b] << " " << j[b] << " " << ans << endl;
+        ans = 0;
     }
-    
 }
