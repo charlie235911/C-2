5bddc4d8d31a989b8a2eb60764ec558e1fcc4205
charlie235911 <biau235911@gmail.com>
2022-03-21 23:59:50 +0800

    add 0321

--- 0321/Q4.c
+++ 0321/Q4.c
@@ -1,30 +1,52 @@
 #include <stdio.h>
-#include <string.h>
+
+long long gcd(long long a, long long b)
+{
+    long long x, y, ans;
+    if (a == b)
+    {
+        x = 0;
+        y = 1;
+        ans = a;
+        printf("%lld %lld %lld\n", x, y, ans);
+    }
+    else
+    {
+        int flag = 0;
+        long long x1 = 1, y1 = 0, x2 = 0, y2 = 1, tempt;
+        while (a % b)
+        {
+            if (flag == 1)
+            {
+                x2 -= a / b * x1;
+                y2 -= a / b * y1;
+                flag = 0;
+            }
+            else
+            {
+                x1 -= a / b * x2;
+                y1 -= a / b * y2;
+                flag = 1;
+            }
+            tempt = a, a = b, b = tempt % b;
+        }
+        if (flag == 1)
+        {
+            printf("%lld %lld %lld\n", x1, y1, b);
+        }
+        else
+        {
+            printf("%lld %lld %lld\n", x2, y2, b);
+        }
+    }
+}
 
 int main(int argc, char const *argv[])
 {
-    int a = 0, b = 0;
-    while(scanf("%d %d", &a, &b) != EOF){
-        char num[100000];
-        int x, y, add, tempt;
-        scanf("%s", &num);
-        for (int i = 0; i < strlen(num); i++)
-        {
-            if (num[i] < 0)
-            {
-                num[i] *= -1;
-            }
-        }
-        for (int i = 0; i < strlen(num) - 1; i++)
-        {
-            for (int j = i + 1; j < strlen(num); j++)
-            {
-                
-                add = i + j;
-            }
-            
-        }
-        
+    long long a = 0, b = 0;
+    while (scanf("%lld %lld", &a, &b) != EOF)
+    {
+        gcd(a, b);
     }
     return 0;
 }
